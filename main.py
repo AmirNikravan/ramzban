2a8efbe4e917f04455bd60f1eec2c6a998bbdb08
Amir <amirnikravan@outlook.com>
2024-09-05 14:28:27 +0330

    show file name and path done

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 from PySide6.QtCore import Qt
 from ui_main import *
-from PySide6.QtWidgets import QMainWindow, QApplication
-import sys
+from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog
+import sys, os
+import datetime
+
 class mainWindow(QMainWindow):
-    def __init__(self, parent= None):
+    def __init__(self, parent=None):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.key = b"0KROAyh6qxSWBF2ZDwRD4Ih1C4kqid7NMMY9kpaxCtY="
+        self.ui.checkBox.stateChanged.connect(self.linestatus)
+        self.ui.pushButton_choosefile_encrypt.clicked.connect(
+            lambda: self.openfile("encrypt")
+        )
+        self.file_encrypt = None
+        self.file_decrypt = None
+        self.ui.textEdit.insertPlainText("logs : \n")
+    def linestatus(self, state):
+        if state == 2:
+            self.ui.lineEdit.clear()
+            self.ui.lineEdit.setDisabled(True)
+        else:
+            self.ui.lineEdit.setDisabled(False)
+
+    def openfile(self, str):
+        if str == "encrypt":
+            self.file_encrypt, _ = QFileDialog.getOpenFileNames(
+                self, "فایل مورد نظر را انتخاب کنید"
+            )
+            self.ui.listWidget_filename_encrpyt.clear()
+            self.ui.listWidget_fileaddress_encrypt.clear()
+            # print(self.file_encrypt)
+            for file_path in self.file_encrypt:
+                file_name = os.path.basename(file_path)
+                self.ui.listWidget_filename_encrpyt.addItem(file_name)
+                self.ui.listWidget_fileaddress_encrypt.addItem(file_path)
+            self.ui.textEdit.insertPlainText(f"{datetime.datetime.now()}\n")
 
 
 if __name__ == "__main__":
--- ui_main.py
+++ ui_main.py
@@ -15,24 +15,24 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QGroupBox, QHBoxLayout,
-    QLabel, QLineEdit, QMainWindow, QPushButton,
-    QSizePolicy, QSpacerItem, QTextEdit, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QGroupBox,
+    QHBoxLayout, QLabel, QLineEdit, QListWidget,
+    QListWidgetItem, QMainWindow, QPushButton, QSizePolicy,
+    QSpacerItem, QTextEdit, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.setEnabled(True)
-        MainWindow.resize(500, 473)
+        MainWindow.resize(600, 500)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QSize(500, 473))
-        MainWindow.setMaximumSize(QSize(500, 473))
+        MainWindow.setMinimumSize(QSize(600, 500))
+        MainWindow.setMaximumSize(QSize(600, 500))
         MainWindow.setStyleSheet(u"background-color: rgb(237, 237, 237);")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
@@ -113,12 +113,17 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.label_filename_decrypt = QLabel(self.groupBox_2)
-        self.label_filename_decrypt.setObjectName(u"label_filename_decrypt")
-        self.label_filename_decrypt.setMaximumSize(QSize(16777215, 16777215))
-        self.label_filename_decrypt.setFont(font2)
+        self.listWidget = QListWidget(self.groupBox_2)
+        self.listWidget.setObjectName(u"listWidget")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.listWidget.sizePolicy().hasHeightForWidth())
+        self.listWidget.setSizePolicy(sizePolicy3)
+        self.listWidget.setMinimumSize(QSize(0, 0))
+        self.listWidget.setMaximumSize(QSize(16777215, 53))
 
-        self.horizontalLayout_4.addWidget(self.label_filename_decrypt)
+        self.horizontalLayout_4.addWidget(self.listWidget)
 
         self.label_8 = QLabel(self.groupBox_2)
         self.label_8.setObjectName(u"label_8")
@@ -133,15 +138,13 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_5 = QHBoxLayout()
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.label_fileaddress_decrypt = QLabel(self.groupBox_2)
-        self.label_fileaddress_decrypt.setObjectName(u"label_fileaddress_decrypt")
-        font3 = QFont()
-        font3.setFamilies([u"Arial"])
-        font3.setPointSize(10)
-        self.label_fileaddress_decrypt.setFont(font3)
-        self.label_fileaddress_decrypt.setWordWrap(True)
+        self.listWidget_2 = QListWidget(self.groupBox_2)
+        self.listWidget_2.setObjectName(u"listWidget_2")
+        sizePolicy3.setHeightForWidth(self.listWidget_2.sizePolicy().hasHeightForWidth())
+        self.listWidget_2.setSizePolicy(sizePolicy3)
+        self.listWidget_2.setMaximumSize(QSize(16777215, 103))
 
-        self.horizontalLayout_5.addWidget(self.label_fileaddress_decrypt)
+        self.horizontalLayout_5.addWidget(self.listWidget_2)
 
         self.label_10 = QLabel(self.groupBox_2)
         self.label_10.setObjectName(u"label_10")
@@ -205,12 +208,18 @@ class Ui_MainWindow(object):
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label_filename_encrypt = QLabel(self.groupBox)
-        self.label_filename_encrypt.setObjectName(u"label_filename_encrypt")
-        self.label_filename_encrypt.setMaximumSize(QSize(16777215, 20))
-        self.label_filename_encrypt.setFont(font2)
+        self.listWidget_filename_encrpyt = QListWidget(self.groupBox)
+        self.listWidget_filename_encrpyt.setObjectName(u"listWidget_filename_encrpyt")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.listWidget_filename_encrpyt.sizePolicy().hasHeightForWidth())
+        self.listWidget_filename_encrpyt.setSizePolicy(sizePolicy4)
+        self.listWidget_filename_encrpyt.setMinimumSize(QSize(0, 53))
+        self.listWidget_filename_encrpyt.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.listWidget_filename_encrpyt.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
-        self.horizontalLayout.addWidget(self.label_filename_encrypt)
+        self.horizontalLayout.addWidget(self.listWidget_filename_encrpyt)
 
         self.label_3 = QLabel(self.groupBox)
         self.label_3.setObjectName(u"label_3")
@@ -225,12 +234,14 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_fileaddress_encrypt = QLabel(self.groupBox)
-        self.label_fileaddress_encrypt.setObjectName(u"label_fileaddress_encrypt")
-        self.label_fileaddress_encrypt.setFont(font3)
-        self.label_fileaddress_encrypt.setWordWrap(True)
+        self.listWidget_fileaddress_encrypt = QListWidget(self.groupBox)
+        self.listWidget_fileaddress_encrypt.setObjectName(u"listWidget_fileaddress_encrypt")
+        sizePolicy4.setHeightForWidth(self.listWidget_fileaddress_encrypt.sizePolicy().hasHeightForWidth())
+        self.listWidget_fileaddress_encrypt.setSizePolicy(sizePolicy4)
+        self.listWidget_fileaddress_encrypt.setMaximumSize(QSize(16777215, 103))
+        self.listWidget_fileaddress_encrypt.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
 
-        self.horizontalLayout_2.addWidget(self.label_fileaddress_encrypt)
+        self.horizontalLayout_2.addWidget(self.listWidget_fileaddress_encrypt)
 
         self.label_5 = QLabel(self.groupBox)
         self.label_5.setObjectName(u"label_5")
@@ -305,16 +316,12 @@ class Ui_MainWindow(object):
         self.checkBox.setText(QCoreApplication.translate("MainWindow", u"\u06a9\u0644\u06cc\u062f \u067e\u06cc\u0634\u0641\u0631\u0636", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u06a9\u0644\u06cc\u062f:", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"\u0631\u0645\u0632\u06af\u0634\u0627\u06cc\u06cc", None))
-        self.label_filename_decrypt.setText("")
         self.label_8.setText(QCoreApplication.translate("MainWindow", u"\u0646\u0627\u0645 \u0641\u0627\u06cc\u0644 :", None))
-        self.label_fileaddress_decrypt.setText("")
         self.label_10.setText(QCoreApplication.translate("MainWindow", u"\u0622\u062f\u0631\u0633 \u0641\u0627\u06cc\u0644:", None))
         self.pushButton_decrypt.setText(QCoreApplication.translate("MainWindow", u"\u0631\u0645\u0632\u06af\u0634\u0627\u06cc\u06cc", None))
         self.pushButton_choosefile_decrpyt.setText(QCoreApplication.translate("MainWindow", u"\u0641\u0627\u06cc\u0644 \u0631\u0627 \u0627\u0646\u062a\u062e\u0627\u0628 \u06a9\u0646\u06cc\u062f", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"\u0631\u0645\u0632\u06af\u0630\u0627\u0631\u06cc", None))
-        self.label_filename_encrypt.setText("")
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u0646\u0627\u0645 \u0641\u0627\u06cc\u0644 :", None))
-        self.label_fileaddress_encrypt.setText("")
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"\u0622\u062f\u0631\u0633 \u0641\u0627\u06cc\u0644:", None))
         self.pushButton_encrypt.setText(QCoreApplication.translate("MainWindow", u"\u0631\u0645\u0632\u06af\u0630\u0627\u0631\u06cc", None))
         self.pushButton_choosefile_encrypt.setText(QCoreApplication.translate("MainWindow", u"\u0641\u0627\u06cc\u0644 \u0631\u0627 \u0627\u0646\u062a\u062e\u0627\u0628 \u06a9\u0646\u06cc\u062f", None))
@@ -322,7 +329,6 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" text-decoration: underline;\">logs:</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
     # retranslateUi
 
